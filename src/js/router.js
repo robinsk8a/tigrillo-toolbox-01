ceeed5fcd9d57cf8a155fa00056fea065522f5a2
Robin <robinsk8a@gmail.com>
2025-04-02 21:51:04 -0500

    Favicon and logo added, Color set updated, Platform layout updated with display grid, logo and title section added

--- src/js/router.js
+++ src/js/router.js
@@ -4,8 +4,8 @@ const routes = {
   '/': './apps/home.html',
   '/checklist': './apps/checklist.html',
   '/generators': './apps/generators.html',
-  '/utilities/slug-normalizer': './apps/slug-normalizer.html',
-  '/utilities/quick-links': './apps/quick-links.html',
+  '/slug-normalizer': './apps/slug-normalizer.html',
+  '/quick-links': './apps/quick-links.html',
   '/test': "./apps/test.html"
 }
 
