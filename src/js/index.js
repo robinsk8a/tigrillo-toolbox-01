7c180c6ceed0e3a7222563aed920d841b557ce0e
Robin <robinsk8a@gmail.com>
2025-04-05 10:51:29 -0500

    Functionality added so only one submeny can be toggle open at a time

--- src/js/index.js
+++ src/js/index.js
@@ -2,20 +2,34 @@ const sidebar = document.querySelector(".sidebar");
 const subMenus = document.querySelectorAll(".submenu");
 
 function toggleSubmenu(element) {
+  // Check if the clicked element is already open
+  if (!element.nextElementSibling.classList.contains("submenu-opened")) {
+    closeAllSubMenus();
+  }
+  // Toggle the clicked submenu
   element.nextElementSibling.classList.toggle("submenu-opened");
+  // If the sidebar is collapsed, toggle it to open
   if (sidebar.classList.contains("sidebar-collapsed")) {
     toggleSidebar(element)
   }
 }
 
 function toggleSidebar(element) {
+  // Toggle the sidebar collapsed class
   sidebar.classList.toggle("sidebar-collapsed");
+  // If the sidebar is collapsed, add the class to the toggle button element to display animatios and close all submenus
   if (sidebar.classList.contains("sidebar-collapsed")) {
     element.classList.add("sidebar-collapsed");
-    subMenus.forEach(subMenu => {
-      subMenu.classList.remove("submenu-opened");
-    });
+    closeAllSubMenus();
   } else {
+    // If the sidebar is open, remove the class from the toggle button element
     element.classList.remove("sidebar-collapsed");
   }
+}
+
+function closeAllSubMenus() {
+  // Close all submenus
+  subMenus.forEach(subMenu => {
+    subMenu.classList.remove("submenu-opened");
+  });
 }
