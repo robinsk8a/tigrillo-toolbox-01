5b2830821dd5d5b36480558fe753e6d9b0ccf15f
Robin <robinsk8a@gmail.com>
2025-04-04 22:33:42 -0500

    Toggle sidebar out when submenu opens and toggle submeuns in when sidebar closes

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,20 @@
+const sidebar = document.querySelector(".sidebar");
+const subMenus = document.querySelectorAll(".submenu");
+
 function toggleSubmenu(element) {
   element.nextElementSibling.classList.toggle("submenu-opened");
+  if (sidebar.classList.contains("sidebar-collapsed")) {
+    toggleSidebar(element)
+  }
 }
 
 function toggleSidebar(element) {
-  const sidebar = document.querySelector(".sidebar");
   sidebar.classList.toggle("sidebar-collapsed");
   if (sidebar.classList.contains("sidebar-collapsed")) {
     element.classList.add("sidebar-collapsed");
+    subMenus.forEach(subMenu => {
+      subMenu.classList.remove("submenu-opened");
+    });
   } else {
     element.classList.remove("sidebar-collapsed");
   }
