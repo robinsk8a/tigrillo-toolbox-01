95f059d8462f391a0def9b1174a8d6358b2eafd6
Robin <robinsk8a@gmail.com>
2025-04-05 11:48:04 -0500

    Sidebar Toggle buton issues when open from submenus when colased solved

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,32 @@
 const sidebar = document.querySelector(".sidebar");
 const subMenus = document.querySelectorAll(".submenu");
+const toggleButton = document.querySelector(".sidebar-toggle");
 
 function toggleSubmenu(element) {
   // Check if the clicked element is already open
   if (!element.nextElementSibling.classList.contains("submenu-opened")) {
-    closeAllSubMenus();
+    closeAllSubMenus(element);
+  }
+  // If the sidebar is collapsed, toggle it to open
+  if (sidebar.classList.contains("sidebar-collapsed")) {
+    toggleSidebar()
   }
   // Toggle the clicked submenu
   element.nextElementSibling.classList.toggle("submenu-opened");
-  // If the sidebar is collapsed, toggle it to open
-  if (sidebar.classList.contains("sidebar-collapsed")) {
-    toggleSidebar(element)
-  }
+  // Toggle the rotate class on the arrow icon
+  element.lastElementChild.classList.toggle("rotate");
 }
 
-function toggleSidebar(element) {
+function toggleSidebar() {
   // Toggle the sidebar collapsed class
   sidebar.classList.toggle("sidebar-collapsed");
   // If the sidebar is collapsed, add the class to the toggle button element to display animatios and close all submenus
   if (sidebar.classList.contains("sidebar-collapsed")) {
-    element.classList.add("sidebar-collapsed");
+    toggleButton.classList.add("sidebar-collapsed");
     closeAllSubMenus();
   } else {
     // If the sidebar is open, remove the class from the toggle button element
-    element.classList.remove("sidebar-collapsed");
+    toggleButton.classList.remove("sidebar-collapsed");
   }
 }
 
@@ -31,5 +34,8 @@ function closeAllSubMenus() {
   // Close all submenus
   subMenus.forEach(subMenu => {
     subMenu.classList.remove("submenu-opened");
+    // Remove the rotate class from all arrow icons
+    subMenu.previousElementSibling.lastElementChild.classList.remove("rotate");
   });
 }
+
