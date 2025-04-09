0ac4133df692b8b3118328fbfa723c5d9c5162b0
Robin <robinsk8a@gmail.com>
2025-04-08 20:56:42 -0500

    Copy to clipboard function created and applied to the Slug normalizer script. Slug normalizer buttons simplified in just one button with a simpler icon

--- src/js/slugNormalizer.js
+++ src/js/slugNormalizer.js
@@ -1,3 +1,5 @@
+import { copyToClipboard } from "./clipboard.js";
+
 function valueToSlug(value) {
   // Trim leading and trailing whitespace
   const startsWithSlash = value.trim().startsWith('/');
@@ -19,32 +21,42 @@ function valueToSlug(value) {
 export function transformToSlug(slugInput, transformBtn = "transform-btn") {
   // Get the input element by ID
   const input = document.getElementById(slugInput);
-  // Get the button element by ID
+
+  // Functional buttons
   const transformButton = document.getElementById(transformBtn);
+  const copyButton = document.getElementById("copy-btn");
+
+
   // Check if the input element exists
   if (!input) {
     return;
   }
   // Get the current value 
   
+  // Slug transformation logic
   function transformToSlug() {
+    // Get the current value of the input field
     const slug = input.value ? valueToSlug(input.value) : '';
-    console.log('Slug generado:', slug);
+    // Copy the slug to the clipboard
+    copyToClipboard(slug);
+    // asign the slug to the input field
     return input.value = slug;
   }
 
+  // Transform and copy on click
   input.addEventListener("keyup", (event) => {
     if (event.key === 'Enter') {
       transformToSlug();
     }
   });
 
+  // Transform and copy on button click
   transformButton.addEventListener("click", (event) => {
     event.preventDefault();
-    const slug = input.value ? valueToSlug(input.value) : '';
-    console.log('Slug generado:', slug);
-    return input.value = slug;
+    transformToSlug();
   });
+
+  
 }
 
 
